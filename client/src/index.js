814eade9380e0ce879e3334113b5e3d59f1b6170
Andrei <amborn02@gmail.com>
2024-05-24 08:43:22 -0500

    Created server backend for connection to mongodb

---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import './index.css';
+import App from './App';
+
+const uri = "mongodb+srv://amborn02:kqGQwjxmrRrKVWWy@cluster0.qq9w1kr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
